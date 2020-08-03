97963000b3ff19aedc9f52f5e79cf84c0108deed
Douile <25043847+Douile@users.noreply.github.com>
2020-08-03 23:38:11 +0100

    Silence camel case warnings
    https://discord.com/developers/docs/resources/channel#channel-object-channel-types

--- src/main.rs
+++ src/main.rs
@@ -60,12 +60,19 @@ struct Guild {
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug, Clone)]
 #[repr(u8)]
 enum ChannelType {
+    #[!warn(non_camel_case_types)]
     GUILD_TEXT = 0,
+    #[!warn(non_camel_case_types)]
     DM = 1,
+    #[!warn(non_camel_case_types)]
     GUILD_VOICE = 2,
+    #[!warn(non_camel_case_types)]
     GROUP_DM = 3,
+    #[!warn(non_camel_case_types)]
     GUILD_CATEGORY = 4,
+    #[!warn(non_camel_case_types)]
     GUILD_NEWS = 5,
+    #[!warn(non_camel_case_types)]
     GUILD_STORE = 6
 }
 
